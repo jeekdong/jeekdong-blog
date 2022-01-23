d86d6f06e80c95a5b5dfed55204cbb74a907a882
jeekdong <jeekdong@gmail.com>
2022-01-23 19:23:18 +0800

    chore: 添加 docker 部署

--- next.config.js
+++ next.config.js
@@ -7,5 +7,8 @@ module.exports = {
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, './');
     return config;
+  },
+  experimental: {
+    outputStandalone: true,
   }
 }
